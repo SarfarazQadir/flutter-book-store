7bc9344b7a8c7adb772768b97fdaf1d14ce78872
Sarfaraz Qadir <sarfarazqadir594@gmail.com>
2025-12-09 06:25:24 +0500

    Add to clarify purpose of body method in BookPage

--- lib/pages/book_page.dart
+++ lib/pages/book_page.dart
@@ -17,6 +17,8 @@ class _BookPageState extends State<BookPage> {
     return body();
   }
 
+  // new method to encapsulate the body of the widget
+
   Widget body() {
     return DefaultTabController(
       length: 2,
